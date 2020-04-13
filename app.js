27149d35a3e81ffa74339b163269901b6dd1d9a1
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-13 18:49:36 -0300

    Sass setup

--- app.js
+++ app.js
@@ -47,6 +47,8 @@ app.use(cookieParser());
 app.use(require('node-sass-middleware')({
     src: path.join(__dirname, 'public'),
     dest:path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
     sourceMap:true
 }));
 
