d4f643ad233d32155b35fc198063379d6ed1382e
Daniel Paiva <danielcspaiva@gmail.com>
2020-04-15 14:58:07 -0300

    added profile avatar so the user is not forced to upload a profile picture

--- routes/controllers/registerControllers.js
+++ routes/controllers/registerControllers.js
@@ -3,29 +3,44 @@ const User = require('../../models/User');
 let registerControllers = {
     //POST /register
     registerPost: (req, res, next) => {
-        let { user } = req.session.passport;
+        let {
+            user
+        } = req.session.passport;
 
         const {
             state,
             address
         } = req.body;
-        
-        const {
-            originalname,
-            url
-        } = req.file;
-        
-        User.findByIdAndUpdate(user, {
-            status: 'registered',
-            state,
-            address,
-            imageName: originalname,
-            imageUrl: url
-        })
-        .then(response => {
-            res.redirect('/dashboard');
-        })
-        .catch(error => console.log(error));
+
+        if (req.file) {
+            const {
+                originalname,
+                url
+            } = req.file;
+            User
+                .findByIdAndUpdate(user, {
+                    status: 'registered',
+                    state,
+                    address,
+                    imageName: originalname,
+                    imageUrl: url
+                })
+                .then(response => {
+                    res.redirect('/dashboard');
+                })
+                .catch(error => console.log(error));
+        } else {
+            User
+                .findByIdAndUpdate(user, {
+                    status: 'registered',
+                    state,
+                    address
+                })
+                .then(response => {
+                    res.redirect('/dashboard');
+                })
+                .catch(error => console.log(error));
+        }
     },
 
     getRegister: (req, res, next) => {
