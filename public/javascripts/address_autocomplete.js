073817b0e3138e2ea53d8819b6e8e3220529e0c1
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-17 22:24:53 -0300

    Fix coordinates issue and add hidden inputs on the dashboard

--- public/javascripts/address_autocomplete.js
+++ public/javascripts/address_autocomplete.js
@@ -7,7 +7,7 @@ let api = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
 let accessToken = '.json?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw&cachebuster=1586829538543&autocomplete=true&types=address';
 addressInput.addEventListener('keydown', (e) => {
     console.log(e.target.value);
-    if(e.target.value.length % 2 === 0){
+
         let data = axios.get(api + e.target.value + accessToken)
         .then( data => {
             let arr = data.data.features;
@@ -19,7 +19,7 @@ addressInput.addEventListener('keydown', (e) => {
             console.log(data)
         })
         .catch(error => console.log(error));
-    }
+
 });
 
 addressInput.addEventListener('blur', function(){
@@ -32,10 +32,10 @@ container.addEventListener('click', function(e){
     addressInput.value = e.target.innerHTML;
     container.innerHTML = "";
 
-    axios.get(api + e.target.value + accessToken)
+    axios.get(api + e.target.innerHTML + accessToken)
     .then( data => {
         let [ lat, long ] = data.data.features[0].center;
-
+        console.log(lat, long, data);
         document.querySelector('input[name="lat"]').value = lat;
         document.querySelector('input[name="lng"]').value = long;
     })
@@ -43,16 +43,3 @@ container.addEventListener('click', function(e){
 
 
 //get coordinates when form is submitted
-
-
-
-
-// let req = 'Rua Jose Ovidio 58';
-// // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,
-// // +Mountain+View,+CA&key=YOUR_API_KEY
-// let apiKeyGoogle = 'AIzaSyBuc7N3UMFZYI7XWFLO96M2SI42IGcIGPY';
-// let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req}&key=${apiKeyGoogle}`
-
-// axios.get(url)
-//  .then( response => console.log(response))
-//  .catch(error => console.log(error));
