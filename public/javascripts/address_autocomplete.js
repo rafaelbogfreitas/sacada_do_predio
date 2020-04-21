eac51546e1d8696738d9bef34946b5e8aa25dc92
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-21 10:01:35 -0300

    Add new API token for mapbox

--- public/javascripts/address_autocomplete.js
+++ public/javascripts/address_autocomplete.js
@@ -6,30 +6,35 @@ console.log(form);
 
 let api = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
 let accessToken = '.json?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw&cachebuster=1586829538543&autocomplete=true&types=address';
-// addressInput.addEventListener('keypress', (e) => {
-//     console.log(e.target.value);
+let sacadaToken = '.json?access_token=pk.eyJ1IjoicmFmYWVsYm9nZnJlaXRhcyIsImEiOiJjazk5d29kencxd3N0M25uMWlvdjludmM3In0.zeXPdzxNYeAo0x9jpu4fJw'
 
-//     axios.get(api + e.target.value + accessToken)
-//         .then( data => {
-//             // console.log(api + e.target.value + accessToken)
-//             let arr = data.data.features;
-//             let options = ''
-//             arr.map( option => {
-//                 options += `<p class="animated fadeInDown">${option.place_name}</p>`;
-//             })
-//             container.innerHTML = options;
-//             console.log(data)
+if(window.innerWidth >= 600) {
 
-//             let [ lat, long ] = data.data.features[0].center;
-//             console.log(lat, long, data);
-//             document.querySelector('.lat').value = lat;
-//             document.querySelector('.lng').value = long;
-
-//         console.log(document.querySelector('.lat'));
-//         })
-//         .catch(error => console.log(error));
-
-// });
+    addressInput.addEventListener('keypress', (e) => {
+        console.log(e.target.value);
+        
+        axios.get(api + e.target.value + accessToken)
+        .then( data => {
+            // console.log(api + e.target.value + accessToken)
+            let arr = data.data.features;
+            let options = ''
+            arr.map( option => {
+                options += `<p class="animated fadeInDown">${option.place_name}</p>`;
+            })
+            container.innerHTML = options;
+            console.log(data)
+            
+            let [ lat, long ] = data.data.features[0].center;
+            console.log(lat, long, data);
+            document.querySelector('.lat').value = lat;
+            document.querySelector('.lng').value = long;
+            
+            console.log(document.querySelector('.lat'));
+        })
+        .catch(error => console.log(error));
+        
+    });
+}
 
 addressInput.addEventListener('blur', function(){
     axios.get(api + addressInput.value + accessToken)
@@ -54,27 +59,30 @@ container.addEventListener('click', function(e){
 });
 
 
+if(window.innerWidth < 600){
 
-addressInput.addEventListener('input', function(){
-    axios.get(api + addressInput.value + accessToken)
-    .then( data => {
-        let [ lat, long ] = data.data.features[0].center;
-        console.log(lat, long, data);
-
-        let arr = data.data.features;
-        let options = ''
-        arr.map( option => {
-            options += `<p class="animated fadeInDown">${option.place_name}</p>`;
-        })
-        container.innerHTML = options;
-
-        document.querySelector('.lat').value = lat;
-        document.querySelector('.lng').value = long;
-
-        console.log(document.querySelector('.lat'));
-        console.log(document.querySelector('.lng'));
+    addressInput.addEventListener('input', function(){
+        console.log('hello')
+        axios.get(api + addressInput.value + sacadaToken)
+        .then( data => {
+            let [ lat, long ] = data.data.features[0].center;
+            console.log(lat, long, data);
+            
+            let arr = data.data.features;
+            let options = ''
+            arr.map( option => {
+                options += `<p class="animated fadeInDown">${option.place_name}</p>`;
+            })
+            container.innerHTML = options;
+            
+            document.querySelector('.lat').value = lat;
+            document.querySelector('.lng').value = long;
+            
+            console.log(document.querySelector('.lat'));
+            console.log(document.querySelector('.lng'));
+        });
     });
-});
+}
 
 form.addEventListener('submit', function(){
     axios.get(api + addressInput.value + accessToken)
