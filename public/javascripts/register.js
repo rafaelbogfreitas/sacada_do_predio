1139e28279f6256ca493a6cf4f5f2e8195f8125f
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-21 21:06:24 -0300

    Add mobile styles for register btn and form

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,11 +1,17 @@
 //Event listener for register btn
 console.log('Aqui')
-let registerBtn = document.querySelector('.register_btn');
+let registrationLink = document.querySelector('.registration_link');
+let registerBtn = document.querySelectorAll('.register_btn');
 let registerForm = document.querySelector('.registration_layer');
 let saveRegisterBtn = document.querySelector('.register_save');
 let closeRegisterLayer = document.querySelector('.close_register');
+console.log(registerBtn)
 
-registerBtn.addEventListener('click', function(){
+registerBtn.forEach((item) => item.addEventListener('click', function(){
+    registerForm.classList.add('translateY');
+}));
+
+registrationLink.addEventListener('click', function(){
     registerForm.classList.add('translateY');
 });
 
