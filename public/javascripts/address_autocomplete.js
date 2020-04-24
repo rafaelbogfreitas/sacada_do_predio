30b938055cabc59e23124541d1c6dbd0a5501399
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-04-23 23:04:30 -0300

    Add geocoder to autocomplete for 'change' event

--- public/javascripts/address_autocomplete.js
+++ public/javascripts/address_autocomplete.js
@@ -7,13 +7,18 @@ let autocomplete = new google.maps.places.Autocomplete(
       types: ['geocode'],
 });
 
+autocomplete.setFields(['geometry']);
+
+let autocompleteRequest = new google.maps.Geocoder();
+let addressInput = document.querySelector('#autocomplete');
+
+// let searchBox = new google.maps.places.SearchBox(addressInput);
 
 autocomplete.addListener('place_changed', onPlaceChanged);
 
 function onPlaceChanged() {
     var place = autocomplete.getPlace();
-
-    console.log(place);
+    console.log(place)
 
     document.querySelector('.lat').value = place.geometry.location.lng();
     document.querySelector('.lng').value = place.geometry.location.lat();
@@ -21,3 +26,30 @@ function onPlaceChanged() {
     console.log(document.querySelector('.lat'));
     
 }
+
+console.log(document.querySelector('.lat').value == "")
+
+//Geocoder
+
+let geocoder = new google.maps.Geocoder();
+
+addressInput.addEventListener('change', function(){
+    if(document.querySelector('.lat').value == '' 
+    || document.querySelector('.lat').value == '' ){
+
+        geocoder.geocode({address: addressInput.value }, function(results, status){
+            console.log(results);
+
+            document.querySelector('.lat').value = results[0].geometry.location.lng();
+            document.querySelector('.lng').value = results[0].geometry.location.lat();
+
+            console.log(document.querySelector('.lat'));
+        });
+
+    }
+})
+
+
+
+
+
